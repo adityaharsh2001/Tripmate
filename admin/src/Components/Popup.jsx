d0ab5ab406872ecc054bdd69da8628987f90da9e
adityaharsh2001 <geekyadi118@gmail.com>
2024-03-01 06:05:36 +0530

    dapp sorting

--- admin/src/Components/Popup.jsx
+++ admin/src/Components/Popup.jsx
@@ -363,7 +363,6 @@ const Popup = ({open, setOpen, categories, packageData = null}) => {
                                         type="text"
                                         name={`description-${index}-${i}`}
                                         id={`description-${index}-${i}`}
-                                        value={description}
                                         className="bg-gray-50 mt-2 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                         placeholder="Enter bullet point"
                                         required=""
