5bc4cd623af8ed98ec118912b7dc6edcbd4b90bb
adityaharsh2001 <geekyadi118@gmail.com>
2024-03-22 02:42:00 +0530

    bahut saare optimizations

--- frontend/src/components/Information.jsx
+++ frontend/src/components/Information.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import PackageForm from "./PackageForm";
-import {useEffect} from "react";
+import ReactMardown from "react-markdown";
 
 const ImagePopup = ({image, closePopup}) => {
     return (
@@ -15,7 +15,6 @@ const ImagePopup = ({image, closePopup}) => {
 
 function Information({packageData, categories}) {
     const [activeImage, setActiveImage] = React.useState(null);
-
     React.useEffect(() => {
         const handleOutsideClick = (event) => {
             if (!event.target.closest(".max-w-full")) {
@@ -31,103 +30,99 @@ function Information({packageData, categories}) {
             document.removeEventListener("click", handleOutsideClick);
         };
     }, [activeImage]);
-
-
-    useEffect(() => {
-        // scan all fields the packageData object and render make any text that is wrapped inside *** *** bold
-        const fields = packageData?.fields;
-        const newFields = fields?.map(field => {
-            const newField = {...field};
-            newField.description = field.description.map(desc => {
-                return desc.replace(/\*\*(.*?)\*\*/g, "<b>$1</b>");
+    const renderDescription = (description) => {
+        //make all text bold that are inside *** ***
+        const boldText = description.match(/\*\*\*(.*?)\*\*\*/g);
+        if (boldText) {
+            boldText.forEach((text) => {
+                description = description.replace(text, `<b>${text.replace(/\*\*\*/g, "")}</b>`);
             });
-            return newField;
-        });
-        packageData.fields = newFields;
-    }, []);
+        }
+        return <div dangerouslySetInnerHTML={{__html: description}}/>;
+    }
+
     const closePopup = () => {
         setActiveImage(null);
     };
     return (
-        <div className="flex flex-col self-center px-9 py-12 bg-white w-full">
-            <div className="flex gap-5 flex-col justify-between mt-7 max-w-full capitalize max-md:flex-wrap">
-                <div className="flex-auto text-3xl font-bold text-indigo-950">
-                    {packageData.name}
-                </div>
-                <div className="flex justify-start gap-5  text-left">
-                    <div className="flex gap-5 text-3xl font-medium text-blue-600">
-                        &#8377;  {packageData.price}
+            <div className="flex flex-col self-center px-9 py-12 bg-white w-full">
+                <div className="flex gap-5 flex-col justify-between mt-7 max-w-full capitalize max-md:flex-wrap">
+                    <div className="flex-auto text-3xl font-bold text-indigo-950">
+                        <ReactMardown>{packageData.name}</ReactMardown>
                     </div>
-                    <div className="my-auto text-base text-zinc-500">Per Person</div>
+                    <div className="flex justify-start gap-5  text-left">
+                        <div className="flex gap-5 text-3xl font-medium text-blue-600">
+                            &#8377;  <ReactMardown>{packageData.price}</ReactMardown>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div
-                className="flex gap-5 mt-5 max-w-full text-base font-medium leading-7 text-center text-gray-500 whitespace-nowrap w-[210px]">
-                <img
-                    loading="lazy"
-                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/1a32bdc706d0cd0fabed9323e4e1cc9d0fb01546f805b207d811e768e11333d7?apiKey=541b99f033084413ad47876370bcfa8c&"
-                    className="flex-1 shrink-0 w-full aspect-[5.88]"
-                />
-                <div className="grow">(2.3k review)</div>
-            </div>
-            <div className="mt-11 text-base leading-7 text-black max-md:mt-10 max-md:max-w-full">
-                {
-                    packageData.description
-                }
-            </div>
-            <div className="mt-16 max-md:mt-10 max-md:max-w-full">
-                <div className="flex flex-wrap gap-5 justify-center max-md:flex-col max-md:gap-0 max-md:">
-                    <div className="flex items-start justify-start flex-col max-md:ml-0 max-md:w-full">
-                        {packageData?.fields?.map((field, index) => (
-                            <>
-                                <div key={index}
-                                     className="flex max-md:flex-col mb-10 max-md:items-start max-md:justify-start items-center justify-center gap-3 md:gap-10 my-auto">
-                                    <div
-                                        className={"text-xl font-bold  text-blue-600 capitalize tracking-[2px]"}>{field.heading}</div>
-                                    {field.description.length < 2 ?
+                <div
+                    className="flex gap-5 mt-5 max-w-full text-base font-medium leading-7 text-center text-gray-500 whitespace-nowrap w-[210px]">
+                    <img
+                        loading="lazy"
+                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/1a32bdc706d0cd0fabed9323e4e1cc9d0fb01546f805b207d811e768e11333d7?apiKey=541b99f033084413ad47876370bcfa8c&"
+                        className="flex-1 shrink-0 w-full aspect-[5.88]"
+                    />
+                    <div className="grow">(2.3k review)</div>
+                </div>
+                <div className="mt-11 text-base leading-7 text-black max-md:mt-10 max-md:max-w-full">
+                    <ReactMardown>{
+                        packageData.description
+                    }</ReactMardown>
+                </div>
+                <div className="mt-16 max-md:mt-10 max-md:max-w-full">
+                    <div className="flex flex-wrap gap-5 justify-center max-md:flex-col max-md:gap-0 max-md:">
+                        <div className="flex items-start justify-start flex-col max-md:ml-0 max-md:w-full">
+                            {packageData?.fields?.map((field, index) => (
+                                <>
+                                    <div key={index}
+                                         className="flex max-md:flex-col mb-10 max-md:items-start max-md:justify-start items-center justify-center gap-3 md:gap-10 my-auto">
                                         <div
-                                            className="text-base items-center justify-center leading-7 text-black">{field.description}</div>
-                                        :
-                                        <ul className="flex flex-col gap-2.5 text-base items-start justify-start leading-7 text-black">
-                                            {field.description.map((desc, index) => (
-                                                <li key={index}>{desc}</li>
-                                            ))}
-                                        </ul>
-                                    }
-                                </div>
-                            </>
-                        ))}
+                                            className={"text-xl font-bold  text-blue-600 capitalize tracking-[2px]"}><ReactMardown>{field.heading}</ReactMardown></div>
+                                        {field.description.length < 2 ?
+                                            <div
+                                                className="text-base items-center justify-center leading-7 text-black"><ReactMardown>{field.description[0]}</ReactMardown></div>
+                                            :
+                                            <ul className="flex flex-col gap-2.5 text-base items-start justify-start leading-7 text-black">
+                                                {field.description.map((desc, index) => (
+                                                    <li key={index}><ReactMardown>{desc}</ReactMardown></li>
+                                                ))}
+                                            </ul>
+                                        }
+                                    </div>
+                                </>
+                            ))}
+                        </div>
+                        <div className="flex flex-col mt-5 max-md:w-full w-1/2">
+                            <PackageForm categories={categories}/>
+                        </div>
                     </div>
-                    <div className="flex flex-col mt-5 max-md:w-full w-1/2">
-                        <PackageForm categories={categories}/>
+                </div>
+                <div
+                    className="mt-32 text-5xl font-bold leading-[71px] text-indigo-950 max-md:mt-10 max-md:max-w-full max-md:text-4xl">
+                    From our gallery
+                </div>
+                <div
+                    className="flex flex-wrap justify-center content-start items-center px-16 py-2.5 mt-12 mr-9 mb-4 max-md:px-5 max-md:mt-10 max-md:mr-2.5 max-md:max-w-full">
+                    <div className="flex flex-col max-w-full w-[990px]">
+                        <div
+                            className="grid cursor-pointer grid-cols-4 gap-5 max-md:grid-cols-2 max-md:gap-0 max-md:grid-rows-2">
+                            {
+                                packageData?.images?.map((image, index) => (
+                                    <img
+                                        onClick={() => setActiveImage(image)}
+                                        key={index}
+                                        loading="lazy"
+                                        src={image}
+                                        className="flex-1 w-full h-full aspect-[1.5]"
+                                    />
+                                ))
+                            }
+                            {activeImage && <ImagePopup image={activeImage} closePopup={closePopup}/>}
+                        </div>
                     </div>
                 </div>
             </div>
-            <div
-                className="mt-32 text-5xl font-bold leading-[71px] text-indigo-950 max-md:mt-10 max-md:max-w-full max-md:text-4xl">
-                From our gallery
-            </div>
-            <div
-                className="flex flex-wrap justify-center content-start items-center px-16 py-2.5 mt-12 mr-9 mb-4 max-md:px-5 max-md:mt-10 max-md:mr-2.5 max-md:max-w-full">
-                <div className="flex flex-col max-w-full w-[990px]">
-                    <div
-                        className="grid cursor-pointer grid-cols-4 gap-5 max-md:grid-cols-2 max-md:gap-0 max-md:grid-rows-2">
-                        {
-                            packageData?.images?.map((image, index) => (
-                                <img
-                                    onClick={() => setActiveImage(image)}
-                                    key={index}
-                                    loading="lazy"
-                                    src={image}
-                                    className="flex-1 w-full h-full aspect-[1.5]"
-                                />
-                            ))
-                        }
-                        {activeImage && <ImagePopup image={activeImage} closePopup={closePopup}/>}
-                    </div>
-                </div>
-            </div>
-        </div>
     );
 }
 
--- frontend/src/components/TourPlan.jsx
+++ frontend/src/components/TourPlan.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import ReactMardown from "react-markdown";
 const TourPlan = ({ packageData }) => {
     return (
         <div className="flex flex-col  self-center px-20 py-12 bg-white w-full">
@@ -9,23 +10,22 @@ const TourPlan = ({ packageData }) => {
                         <div key={index} className="timeline__block">
                             <div className="timeline__midpoint"></div>
                             <div className={`timeline__content ${index % 2 === 0 ? 'timeline__content--left' : 'timeline__content--right'}`}>
-                                <h3 className="timeline__year">Day {day.day}</h3>
+                                <h3 className="timeline__year">Day <ReactMardown>{day.day}</ReactMardown></h3>
                                 <div className="flex flex-col gap-2.5 mb-5">
                                     <div className="flex flex-row items-center justify-start gap-2">
-                                        <span className="text-base font-normal">{day.title}</span>
+                                        <span className="text-base font-normal"><ReactMardown>{day.title}</ReactMardown></span>
                                     </div>
                                     {
                                         day.description.length < 2 ? (
                                             <ul className="flex inside-point flex-col text-sm items-start justify-start gap-2">
-                                                <li>{day.description}</li>
+                                                <li><ReactMardown>{day.description}</ReactMardown></li>
                                             </ul>
                                         ) : day.description.map((desc, index) => (
                                             <ul key={index} className="flex inside-point flex-col text-sm items-start justify-start gap-2">
-                                                <li>{desc}</li>
+                                                <li><ReactMardown>{desc}</ReactMardown></li>
                                             </ul>
                                         ))
                                     }
-
                                 </div>
                             </div>
                         </div>
